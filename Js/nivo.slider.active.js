0e57f355b268c3e5c230a9019dc006d2f64b66b3
G-ars <gustavoaraujo.job@hotmail.com>
2020-09-02 15:28:11 -0300

    Projeto concluido

---
+++ Js/nivo.slider.active.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+	/*--------------------------------------
+		Nivo Slider Active
+	----------------------------------------*/
+	$('.slider-active').nivoSlider({
+		controlNav: false,
+		directionNav: false,
+		randomStart: true,
+		controlNavThumbs: false,
+		animSpeed: 500,
+		pauseTime: 5000,
+		pauseOnHover: false,
+		manualAdvance: false,
+		slices: 15,
+		boxCols: 8,
+		boxRows: 4,
+	});
+
+})(jQuery);	
